4ce69bf83df4835224645d31e98096d7542c019c
diznq <jlackovic98@gmail.com>
2017-10-21 10:35:06 +0200

    tchar fix

--- src/cpp/precheck/main.cpp
+++ src/cpp/precheck/main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <Shlwapi.h>
+#include <tchar.h>
 
 #pragma comment(lib, "Shlwapi")
 
@@ -8,9 +9,8 @@ int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR param, int) {
 	TCHAR path[MAX_PATH];
 	TCHAR params[MAX_PATH + 100];
 	GetCurrentDirectory(MAX_PATH, path);
-	PathAppendW(path, L"..\\Game\\");
-	swprintf(params, L"\"%s\\\" /grant Users:(OI)(CI)F /T", path);
-	MessageBoxW(0, params, 0, 0);
-	ShellExecuteW(NULL, NULL, L"icacls.exe", params, NULL, SW_NORMAL);
+	PathAppend(path, TEXT("..\\Game\\"));
+	_stprintf(params, TEXT("\"%s\\\" /grant Users:(OI)(CI)F /T"), path);
+	ShellExecute(NULL, NULL, TEXT("icacls.exe"), params, NULL, SW_NORMAL);
 	return 0;
 }
